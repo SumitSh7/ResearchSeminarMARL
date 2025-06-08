da1e546e0061639b376330d37b42932e85d5a469
SumitSh7 <sumitsh2012@gmail.com>
2025-06-08 14:21:22 +0200

    Seminar Project Beginning

---
+++ main.py
@@ -0,0 +1,11 @@
+import pettingzoo
+
+def print_hi(name):
+
+    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+    print(f"Petting Zoo Version: {pettingzoo.__version__}")
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
